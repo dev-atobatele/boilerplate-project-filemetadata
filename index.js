c139ed1ede65af2d566f2914d5cd2fac88879992
Tim Atobatele <dev.atobatele@gmail.com>
2024-05-22 15:43:47 +0000

    File Metadata Microservice : Complete

--- index.js
+++ index.js
@@ -7,12 +7,24 @@ var app = express();
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+let multer = require('multer');
+let upload = multer({dest:'uploads/'})
+
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), function(req,res){
+  let file = req.file;
+  let resObj = {
+    name:file.originalname,
+    type:file.mimetype,
+    size:file.size
+  };
+  console.log(resObj);
+  res.json(resObj);
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
